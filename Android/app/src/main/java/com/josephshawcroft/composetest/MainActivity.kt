0095981d53f5120a175d05a4204ca65d50a8b660
Joseph Shawcroft <joeymoto@hotmail.co.uk>
2021-02-27 15:09:10 +0000

    Added column and image

--- Android/app/src/main/java/com/josephshawcroft/composetest/MainActivity.kt
+++ Android/app/src/main/java/com/josephshawcroft/composetest/MainActivity.kt
@@ -3,14 +3,37 @@ package com.josephshawcroft.composetest
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContent {
-            Text("Hello world!")
+            Column(
+                modifier = Modifier.padding(16.dp)
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.header),
+                    contentDescription = null
+                )
+
+                Greeting(name = "Android")
+                Text("Test")
+                Text("Lol")
+            }
         }
     }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
 }
